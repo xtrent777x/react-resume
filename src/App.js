cc1703f851f10e9aa125f63e15c7eaa1b79acb3d
Trent Roesbery <xtrent777x@gmail.com>
2021-10-22 17:47:42 -0600

    re structure

--- src/App.js
+++ src/App.js
@@ -1,48 +1,6 @@
-// import React, { useState } from 'react';
-// import Nav from './components/Nav';
-// import About from './components/About';
-// import Gallery from './components/Gallery';
-// import ContactForm from './components/Contact';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  const [categories] = useState([
-    {
-      name: 'Resume',
-      description: 'Click for my resume',
-    },
-    { name: 'Portfolio', 
-      description: 'See My current projects' 
-    },
-  
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
-
-  return (
-    <div>
-      Trent Roesbery Portfolio
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            <Gallery currentCategory={currentCategory}></Gallery>
-            <About></About>
-          </>
-        ):
-        (
-            <ContactForm></ContactForm>
-          )}
-      </main>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
