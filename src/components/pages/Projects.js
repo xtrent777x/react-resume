7f3b3d0f5bf37bb99b5db3877a52d6afbd671197
Trent Roesbery <xtrent777x@gmail.com>
2021-10-22 18:12:00 -0600

    projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,1 +1,12 @@
-/// base file ///
+import React from "react";
+
+const Projects = () => (
+  <div>
+    <h1>Projects Page</h1>
+    <p>
+      See and click on Some of my completed projects
+    </p>
+  </div>
+);
+
+export default Projects;
